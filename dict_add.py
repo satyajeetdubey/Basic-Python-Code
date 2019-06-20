6be52fff66b34edddd74a9b04b6282db092cd844
satyajeetdubey <45504683+satyajeetdubey@users.noreply.github.com>
2019-06-21 00:27:28 +0530

    Add files via upload

---
+++ dict_add.py
@@ -0,0 +1,6 @@
+'''Write a Python script to add a key to a dictionary
+Sample Dictionary : {0: 10, 1: 20}
+Expected Result : {0: 10, 1: 20, 2: 30}'''
+samp_dict={0:10,1:20}
+samp_dict[2]=30
+print(samp_dict)
