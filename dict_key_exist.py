629050be9f18d1cbdfd9a71f11cfbc39f50bffc5
satyajeetdubey <45504683+satyajeetdubey@users.noreply.github.com>
2019-06-21 23:30:23 +0530

    Add files via upload

---
+++ dict_key_exist.py
@@ -0,0 +1,8 @@
+#Write a Python script to check if a given key already exists in a dictionary.
+dict1={'a':'hello','age':'23','name':'satyajet'}
+
+a=input('Enter a no. :')
+if a in dict1:
+    print("KEy is exist")
+else:
+    print("Key not exist")
