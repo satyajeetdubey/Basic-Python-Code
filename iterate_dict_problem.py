5a8ff57bce3c85a72f2a1d5febed25faad262161
satyajeetdubey <45504683+satyajeetdubey@users.noreply.github.com>
2019-06-23 16:35:22 +0530

    Add files via upload

---
+++ iterate_dict_problem.py
@@ -0,0 +1,27 @@
+# Write a Python program to iterate over dictionaries using for loops.
+d={'name':'satyajeet kumar dubey','age':19,'adress':'ballia'}
+
+
+
+#for k,v in d.items():
+ #   print(k,v)
+
+
+ 
+#second method:---
+
+#for i in d.items():
+#    print(i)
+
+
+
+#third method:----
+
+
+for k in d:
+    print(k,d.get(k))
+
+
+
+
+
