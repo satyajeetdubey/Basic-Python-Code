34441b906537507443344dbcd7ae862ce811a6b6
satyajeetdubey <45504683+satyajeetdubey@users.noreply.github.com>
2019-07-02 00:18:27 +0530

    Add files via upload

---
+++ dict_int_squrt.py
@@ -0,0 +1,14 @@
+#key=int(input("Enter a key"))
+n=int(input('enter a no. '))
+
+dict1={}
+
+for i in range(1,n+1):
+    dict1[i]=i*i
+  #  for j in range(key+1):
+   #     dict1[j]=j*j
+
+
+
+print(dict1)
+    
