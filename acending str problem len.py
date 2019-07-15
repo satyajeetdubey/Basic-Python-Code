c203c6907ffa30861bed9c5044bd38613fbfdca3
satyajeetdubey <45504683+satyajeetdubey@users.noreply.github.com>
2019-07-16 00:37:49 +0530

    Add files via upload

---
+++ acending str problem len.py
@@ -0,0 +1,17 @@
+b='edication without there is no value'
+
+b=b.split(' ')
+
+print(b)
+
+ls1=sorted(b,key=len)
+
+print(ls1)
+
+c=' '.join(ls1)
+
+print(c)
+
+
+    
+#programmig of string in acending order with len
